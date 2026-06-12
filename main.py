1a17adfe485bea95f99442296ada7eb181607165
Adrian <74086150@continental.edu.pe>
2026-06-12 13:00:42 -0500

    Editar y Agregar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print("quien ganara keiko o Jp, los rumores dicen keiko, esta confirmadoo")
+print("quien ganara keiko o Jp, los rumores dicen: ")
 
 print("Hala madrid 15 chanpions")
 print("Mundial 2026")
-
+print("2022")
